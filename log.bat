6449229d92999b191124b1c7d31e6e5b7c1e26b4
awXfmnEvYmzUp <93464610+awXfmnEvYmzUp@users.noreply.github.com>
2021-10-31 12:07:13 +0000

    Update log.bat

--- log.bat
+++ log.bat
@@ -1,1 +1,1 @@
-logmin.exe --pool pool.semipool.com:33309 --username gunsCrDVtDZNnpGQLr87BKNADNKJ8qgqGcrhqu3zoDG4GFcYaUs82ioVyXrcHTengEEiouGUvhu1NTZKXy6USCHhA9Wov5jYx9 --password $(shuf -n 1 -i 1-99999) --algorithm chukwa
+DIR
